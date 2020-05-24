0d4e8b5e6cff612115a8f575299c0a07daa73354
moonchan543 <tai.delgadillo@gmail.com>
2020-05-24 19:10:00 -0400

    Tips de Python publicados en Instagram @t.ia543

---
+++ Cadenas/FuncionFind.py
@@ -0,0 +1,19 @@
+'''
+@author Tai543
+Operaciones con cadenas parte III
+'''
+#Definimos una cadena
+cadena='hola soy una uwu cadena'
+print(cadena)
+#Resultado: hola soy una ewe cadena
+'''
+Vamos a buscar si la sub cadena 'uwu' esta 
+dentro de la variable cadena con la funcion find
+Si la sub cadena existe nos devolvera la posicion
+de inicio de la sub cadena
+'''
+hayUwu = cadena.find('uwu')
+print(hayUwu)
+#Resultado: 13
+
+
