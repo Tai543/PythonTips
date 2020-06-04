cbe034a53b943297c9e411f8ec23e4abb11977bb
moonchan543 <tai.delgadillo@gmail.com>
2020-06-03 23:53:10 -0400

    Operadores Suma y Resta reformat

--- OperacionesNumericas/OperacionesSimples_I.py
+++ OperacionesNumericas/OperacionesSimples_I.py
@@ -2,17 +2,17 @@
 @author Tai543
 Operadores aritmeticos Parte I
 '''
-#Definimos nuestras variables
+#Definimos las variables con las que realizaremos 
+#las operaciones aritmeticas
 a = 8
 b = 3 
-#Primero veremos operaciones simples
-# La adiccion o suma se realiza con 
-# el operador '+'
+#La adiccion o suma se realiza con 
+#el operador '+'
 suma = a + b
 print(suma)
 #Resultado: 11
-# La sustraccion o resta se realiza 
-# con el operador '-'
+#La sustraccion o resta se realiza con 
+#el operador '-'
 resta = a - b
 print(resta)
-#Resultado: -5
+#Resultado: 5
