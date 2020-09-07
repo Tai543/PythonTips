423f06d0abacf668e60c33ab86634d3da08594e5
Tai543 <tai.delgadillo@gmail.com>
2020-09-07 01:49:35 -0400

    Quiz

--- Listas/Segmentacion.py
+++ Listas/Segmentacion.py
@@ -1,36 +1,12 @@
 '''
 @author Tai543
-Listas: Indices
-'''
-# Definamos la lista 'python'
-python = ['P','Y','T','H','O','N']
-# Los indices de los elementos de la lista 
-# 'python' se pueden leer de las siguientes formas 
-'''
-+---+---+---+---+---+---+
-| P | Y | T | H | O | N |
-+---+---+---+---+---+---+           indices
-| 0 | 1 | 2 | 3 | 4 | 5 | <<-- izquierda a derecha
-+---+---+---+---+---+---+           indices
-|-6 |-5 |-4 |-3 |-2 |-1 | <<-- derecha a izquierda
-+---+---+---+---+---+---+
-'''
-# Mostremos el primer elemento de la lista 'python'
-# usando ambos tipos de indices 😎👩🏻‍🏫👩🏻‍💻
-print(python[0])
-# Resultado: P
-print(python[-6])
-# Resultado: P
-
-'''
-@author Tai543
 Listas: Segmentacion
-'''
-# La segmentacion es una propiedad muy cool de las 
-# listas en python 😎👩🏻‍🏫👩🏻‍💻 para usarla solo tenemos 
-# que seguir la siguiente notacion '[inicio:fin]' 
-# 'inicio' --  indice donde inicia el segmento
-# 'fin' -- indice donde finiliza el segmento + 1    
+La segmentacion es una propiedad muy cool de las 
+listas en python 😎👩🏻‍🏫👩🏻‍💻 para usarla solo tenemos 
+que seguir la siguiente notacion '[inicio:fin]' 
+'inicio' --  indice donde inicia el segmento
+'fin' -- indice donde finiliza el segmento + 1
+'''    
 # Vamos a definir la lista 'python'  
 python = ['P','Y','T','H','O','N']
 print(python)
--- QuickQuiz/QuizListas.py
+++ QuickQuiz/QuizListas.py
@@ -5,14 +5,10 @@ Quick Quiz de Listas
 python = [1,2,3,4,5,6]
 print(python[2:5])
 
-
-
-
-
-
-
-
-
-
-
+'''
+@author Tai543
+Quick Quiz de Listas
+'''
+python = [1,2,3,4,5,6]
+print(python[-3:-1])
 
